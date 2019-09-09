10ef4a862b5cfe816562ef10ef75e6bae713e8bd
Piotr Zegota <piotr.zegota@idemia.com>
2019-09-09 18:36:38 +0200

    added translation and tel hashing

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,4 @@
-FROM nginx
-
+FROM nginx:alpine
 LABEL maintainer="SAIL-IT.PRO"
-
 EXPOSE 80
-
 COPY ./ /usr/share/nginx/html
--- js/main.js
+++ js/main.js
@@ -10,3 +10,14 @@ jQuery(document).ready(function($) {
         });
 
 });
+
+var mparts = ["kontakt", "sail-it", "pro", ".", "@"];
+var mhashed = mparts[0] + mparts[4] + mparts[1] + mparts[3] + mparts[2];
+document.getElementById("mhashed2").innerHTML=mhashed;
+document.getElementById("mhashed1").href="mailto:"+mhashed;
+
+var tparts = ["727", "+48", "610", "510", " "];
+var thashed1 = tparts[1] + tparts[4] + tparts[3] + tparts[4] + tparts[0] + tparts[2];
+var thashed2 = tparts[1] + tparts[3] + tparts[0] + tparts[2];
+document.getElementById("thashed1").innerHTML=thashed1;
+document.getElementById("thashed1").href="tel:"+thashed2;
