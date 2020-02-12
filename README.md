e5f5ef2b907d7afb4716b9ea02f04cc8e94c6fb1
Piotr Zegota <piotr.zegota@idemia.com>
2020-02-12 14:35:19 +0100

    purge main

--- README.md
+++
@@ -1,1 +0,0 @@
-# zegalpl.github.io
--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker rm -f website
-docker rmi website
-docker build -t website .
-docker run -d -p 80:80 --name website website
-xdg-open http://localhost:80 </dev/null >/dev/null 2>&1 & disown
